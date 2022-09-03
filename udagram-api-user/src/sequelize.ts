711d46be6e34526fe4d2fbfdacb8ef345181055f
ahmedHamdy203 <ahmed_hamdy22317@outlook.com>
2022-09-03 13:19:19 -0400

    sequalize modification in user

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -2,12 +2,13 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
 
-export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+// export const sequelize = new Sequelize({
+//   'username': config.username,
+//   'password': config.password,
+//   'database': config.database,
+//   'host': config.host,
 
-  'dialect': config.dialect,
-  'storage': ':memory:',
-});
+//   'dialect': config.dialect,
+//   'storage': ':memory:',
+// });
+export const sequelize = Sequelize(`postgres://${config.username}:${config.password}@${config.host}:5432/postgres`);
