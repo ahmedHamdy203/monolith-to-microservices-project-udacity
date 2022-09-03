118f8177a448ed14af21b7df88e9814cf8613ee7
ahmedHamdy203 <ahmed_hamdy22317@outlook.com>
2022-09-03 18:17:40 -0400

    deployment files

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -2,13 +2,13 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
 
-// export const sequelize = new Sequelize({
-//   'username': config.username,
-//   'password': config.password,
-//   'database': config.database,
-//   'host': config.host,
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
 
-//   'dialect': config.dialect,
-//   'storage': ':memory:',
-// });
-export const sequelize = new Sequelize(`postgres://hamdy:test1234@p3-db6-dev.ct8dw2y940hp.us-east-1.rds.amazonaws.com:5432/postgres`);
+  'dialect': config.dialect,
+  'storage': ':memory:',
+});
+// export const sequelize = new Sequelize(`postgres://hamdy:test1234@p3-db6-dev.ct8dw2y940hp.us-east-1.rds.amazonaws.com:5432/postgres`);
